9f8458ec736ef395d5034363b70cd46abe4df020
JdotCam <j.camarillo1000@gmail.com>
2026-03-22 19:50:50 -0700

    Added splash <section> with a placeholder image, title, description, and the Shop Products button (SCRUM-3)

---
+++ frontend/src/components/ui/PrimaryButton.vue
@@ -0,0 +1,23 @@
+<template>
+    <button class="primary-btn">
+        <slot></slot>
+    </button>
+</template> 
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+.primary-btn {
+    background-color: #FFF;
+    color: #264DC1;
+    border: none;
+    padding: 10px 20px;
+    border-radius: 10px;
+    cursor: pointer;
+}
+
+.primary-btn:hover {
+    background-color: #DDD;
+}
+</style>
--- frontend/src/views/Ecommerce.vue
+++ frontend/src/views/Ecommerce.vue
@@ -1,12 +1,19 @@
 <template>
   <admin-layout>
-    <div class="grid grid-cols-12 gap-4 md:gap-6">
-      <!-- SCRUM-3 - Add button to navigate to Products page.  This should be on the splash page. -->
-      <!-- Temporary splash section-->
-      <div class="col-span-12 px-4 py-10 bg-[#170925]">   
-        <primary-button @click="goToProducts">Shop Products</primary-button> 
+    <!-- SCRUM-44 - Splash section.  This should be on the splash page. -->
+    <section class="bg-[linear-gradient(to_bottom_right,rgba(0,0,95,0.5),rgba(63,0,95,0.5)),url('https://images.unsplash.com/photo-1760986784449-79e1a6ba616b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxjb21pYyUyMGJvb2slMjBzdG9yZSUyMGludGVyaW9yfGVufDF8fHx8MTc3MjQwNzU3MXww&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral')] bg-cover bg-center">
+      <div class="col-span-12 flex items-center mx-auto px-4 py-10">
+          <div class="w-2/3">
+              <h2 class="text-5xl mb-4 font-bold text-white">Welcome to Skyscraper Comics</h2>
+              <p class="text-xl text-gray-200 mb-8">Discover your next favorite comic book or master your favorite trading card game. Where collectors and players come together.</p>
+              <!-- SCRUM-3 - Add button to navigate to Products page.  This should be on the splash page. -->
+              <primary-button>Shop Products</primary-button> 
+              <!-- SCRUM-3 -->
+          </div>
       </div>
-      <!-- SCRUM-3 -->
+    </section>
+    <!-- SCRUM-44 -->
+    <div class="grid grid-cols-12 gap-4 md:gap-6">
       <div class="col-span-12 space-y-6 xl:col-span-7">
         <ecommerce-metrics />
         <monthly-target />
