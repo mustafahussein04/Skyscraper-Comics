e0109a431eb8b6aec5f3226db2b3298f4553cae2
JdotCam <j.camarillo1000@gmail.com>
2026-04-23 20:09:25 -0700

    Added a search bar, which is typable, and added extra tester buttons to show how the search bar scales with buttons (#15)

--- frontend/src/views/ProductsView.vue
+++ frontend/src/views/ProductsView.vue
@@ -1,7 +1,17 @@
 <template>
     <div>
         <h1>Our Products</h1>
-        <PrimaryButton>Test Button</PrimaryButton>
+        <!-- SCRUM-5 - Search bar, and other tester buttons -->
+        <div class="mb-8 bg-white p-6 rounded-lg shadow-sm">
+            <div class="flex flex-col flex-row gap-4">
+                <input type="text" placeholder="Search products..." class="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-900" value>
+                <PrimaryButton>Test</PrimaryButton>
+                <PrimaryButton>Test</PrimaryButton>
+                <PrimaryButton>Test</PrimaryButton>
+                <PrimaryButton>Test</PrimaryButton>
+            </div>
+        </div>
+        <!-- SCRUM-5-->
     </div>
 </template>
 
