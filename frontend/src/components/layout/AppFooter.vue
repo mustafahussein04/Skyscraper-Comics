7c5e0e72250f95df3c5c85a98fd9819189ab488f
brc2004 <brchisum@csus.edu>
2026-05-08 09:34:01 -0700

    SCRUMS 142, 143, & 146 (#25)

--- frontend/src/components/layout/AppFooter.vue
+++ frontend/src/components/layout/AppFooter.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="bg-gray-900 text-white px-6 py-12">
-    <div class="grid grid-cols-1 sm:grid-cols-3 gap-8 max-w-5xl mx-auto mb-8">
+    <div class="grid grid-cols-1 sm:grid-cols-4 gap-8 max-w-6xl mx-auto mb-8">
       <div>
         <h4 class="font-bold mb-2">Skyscraper Comics</h4>
           <p class="text-gray-400 text-sm">Here in Natomas for your Comic and Trading Card game needs! Buy • Sell • Trade!!</p>
@@ -15,6 +15,11 @@
           <p class="text-gray-400 text-sm">Sacramento, California 95833</p>
           <p class="text-gray-400 text-sm">(916) 515-8293</p>
           <p class="text-gray-400 text-sm">skyscrapercomics1@gmail.com</p>
+      </div>
+      <div>
+        <h4 class="font-bold mb-2">Social Media</h4>
+          <p class="text-gray-400 text-sm">Facebook: <a href="https://www.facebook.com/Skyscrapercomics/" target="_blank" rel="noopener noreferrer" class="hover:text-white underline">Skyscrapercomics</a></p>
+          <p class="text-gray-400 text-sm">Instagram: <a href="https://www.instagram.com/skyscraper_comics/" target="_blank" rel="noopener noreferrer" class="hover:text-white underline">@skyscraper_comics</a></p>
       </div>
     </div>
     <div class="border-t border-gray-700 pt-6">
