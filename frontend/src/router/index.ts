1dadc3953d27c370ea076d0933fc9e0098f6e131
Ethan <ethanjordan@csus.edu>
2026-09-13 17:21:16 -0700

    restored header and footer

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -35,13 +35,13 @@ const router = createRouter({
             title: 'Product Detail',
           },
         },
+        {
+          path: 'products/:filter?',
+          name: 'Products',
+          component: () => import('../views/ProductsView.vue'),
+          props: {},
+        },
       ],
-    },
-    {
-      path: '/products/:filter?',
-      name: 'Products',
-      component: () => import('../views/ProductsView.vue'),
-      props: {},
     },
     {
       path: '/calendar',
